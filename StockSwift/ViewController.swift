9cd48f523d89a6517ed345fd2b2aad98854edf72
DoctorCobweb <spinninghalf@gmail.com>
2015-12-27 15:44:17 +1100

    Initial Commit

---
+++ StockSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StockSwift
+//
+//  Created by andre trosky on 27/12/2015.
+//  Copyright © 2015 andre trosky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
