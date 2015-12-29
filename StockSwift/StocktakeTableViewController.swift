259833d883ebed0192f50d65eb27e5f8c07332c6
DoctorCobweb <spinninghalf@gmail.com>
2015-12-29 12:00:01 +1100

    change the search bar scope functionality

--- StockSwift/StocktakeTableViewController.swift
+++ StockSwift/StocktakeTableViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
+
+//this is the class extension which makes StocktakeTableViewController conform
+//to UISearchResultsUpdating.
+//updateSearchResultsForSearchController is the only method that the class MUST
+//implement to conform to the protocol
+//whenever the user adds/removes text in the search bar, the UISearchController will
+//inform StocktakeTableViewController class of the change via this method.
+//the custom logic of how to handle the filtering of search is done using 
+//the filterContentForSearchText func.
 extension StocktakeTableViewController: UISearchResultsUpdating {
     func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+        //get the search bar
         let searchBar = searchController.searchBar
+        
+        //get the search bar scope value selected. scope buttons are the catogories
+        //of stock
         let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+        
+        //finally, call the custom func to filter search results and update UI
         filterContentForSearchText(searchController.searchBar.text!, scope:scope)
     }
 }
@@ -49,23 +65,56 @@ class StocktakeTableViewController: UITableViewController{
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
         
-        loadStocktakeItems()
         
+        
+        //searchResultsUpdater is a property that conforms to the new protocol, UISearchResultsUpdating.
+        //this class is assigned to it as we add the protocol methods using a class extension defined up top of file.
         searchController.searchResultsUpdater = self
+        
+        //dim view it is presented over which is the tableview. we don't want it dimmed
         searchController.dimsBackgroundDuringPresentation = false
+        
+        //make sure sesarch bar does not remain on screen if user navigates to another view controller
+        //whilst the UISearchController is active
         definesPresentationContext = true
+        
+        //interface builder is not yet compatible with UISearchController so we add it programmatically.
         tableView.tableHeaderView = searchController.searchBar
         
         
-        searchController.searchBar.scopeButtonTitles = ["ALL", "MEAT", "GROCERY", "PRODUCE", "DESSERT", "OTHER"]
+        searchController.searchBar.scopeButtonTitles = ["ALL", "MEAT", "GROCERY", "PRODUCE", "OTHER"]
         searchController.searchBar.delegate = self
+        
+        //finally load in the stock items
+        loadStocktakeItems()
     }
     
     func filterContentForSearchText(searchText: String, scope: String = "ALL") {
+        print("filterContentForSearchText and searchText: " + searchText)
+        
         //this filters stockItems array based on the searchText string and will put the results into filteredStockItems array.
+        //
+        //filter() takes a closure of type (item:StockItem) -> Bool
+        //then it loops over all elements of the array, calls the closure passing in the current element,
+        //if true is returned for that element it is added to filteredStockItems
         filteredStockItems = stockItems.filter { item in
-            let categoryMatch = (scope == "ALL") || (item.section == scope)
-            return categoryMatch && item.description.lowercaseString.containsString(searchText.lowercaseString)
+            
+            if searchText == "" && scope != "ALL" {
+                //print("filterContentForSearchText, searchText == \"\" && scope != ALL is true")
+                return item.section == scope
+            }
+            else if searchText == "" && scope == "ALL" {
+                //want all of the items
+                return true
+            }
+            else if searchText != "" && scope == "ALL" {
+                return item.description.lowercaseString.containsString(searchText.lowercaseString)
+            }
+            else {
+                //print("filterContentForSearchText, searchText != \"\" is true")
+                //let categoryMatch = (scope == "ALL") || (item.section == scope)
+                return (item.section == scope) && item.description.lowercaseString.containsString(searchText.lowercaseString)
+            }
         }
         
         tableView.reloadData()
@@ -85,6 +134,8 @@ class StocktakeTableViewController: UITableViewController{
             print(item.description)
             print(item.inv_code)
             print(item.last_cost!)
+            print(item.stock_group_cleaned)
+            print(item.stock_group_cleaned.characters.count)
             
             let photo:UIImage?
             
@@ -182,14 +233,31 @@ class StocktakeTableViewController: UITableViewController{
                         headers = values
                     }
                     else {
+                        
+                        //trim whitespace and newlines from strings
+                        let stock_group_tmp = values[0].stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+                        var stock_group_cleaned_tmp = values[1].stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+                        let description_tmp = values[3].stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+                        let units_tmp = values[4].stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+                        let barcode_tmp = values[6].stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+                        
+                        
+                        //change stock group categories to make a smaller number of categories.
+                        //need to do this so the search bar scope terms are not too many and hence display bad/small
+                        let meat_set: Set = ["POULTRY", "BEEF", "PORK", "LAMB", "SEAFODD"]
+                        let other_set: Set = ["DESSERT", "BEVERAGE"]
+                        stock_group_cleaned_tmp = meat_set.contains(stock_group_cleaned_tmp) ? "MEAT" : stock_group_cleaned_tmp
+                        stock_group_cleaned_tmp = other_set.contains(stock_group_cleaned_tmp) ? "OTHER" : stock_group_cleaned_tmp
+                        
+                        
                         // Put the values into the tuple and add it to the items array
-                        let item = (stock_group: values[0],
-                                    stock_group_cleaned: values[1],
+                        let item = (stock_group: stock_group_tmp ,
+                                    stock_group_cleaned: stock_group_cleaned_tmp,
                                     inv_code: Int(values[2]),
-                                    description: values[3],
-                                    units: values[4],
+                                    description: description_tmp,
+                                    units: units_tmp,
                                     last_cost: Float(values[5]),
-                                    barcode: values[6])
+                                    barcode: barcode_tmp)
                         items?.append(item)
                     
                     }
@@ -205,24 +273,6 @@ class StocktakeTableViewController: UITableViewController{
 
     
     
-    /*
-    //OLD NOT USED
-    func getContentsOfURL() -> String? {
-        if let path = NSBundle.mainBundle().pathForResource("assets/stock_data", ofType: "csv") {
-            do {
-
-                return try String(contentsOfURL: NSURL(fileURLWithPath: path), encoding: NSUTF8StringEncoding) as String
-                //return try String(contentsOfFile: "stock_data", encoding:NSUTF8StringEncoding) as String
-            } catch {
-                print("caught a nil in getContentsOfURL")
-                return nil
-            }
-        }
-        else {
-            return nil
-        }
-    }
-    */
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -238,9 +288,27 @@ class StocktakeTableViewController: UITableViewController{
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        if searchController.active && searchController.searchBar.text != "" {
-            return filteredStockItems.count
-        } else {
+        
+        //find out how many rows to display factoring into account what is happening to the search bar content
+        if searchController.active {
+            let searchBar = searchController.searchBar
+            let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+            
+            if searchBar.text == "" && scope != "ALL" {
+                return filteredStockItems.count
+            }
+            else if searchBar.text == "" && scope == "ALL" {
+                return stockItems.count
+            }
+            else if searchBar.text != "" && scope == "ALL" {
+                return filteredStockItems.count
+            }
+            else {
+                //searchBar.text != "" && scope != "ALL" is true
+                return filteredStockItems.count
+            }
+        }
+        else {
             return stockItems.count
         }
     }
@@ -250,23 +318,35 @@ class StocktakeTableViewController: UITableViewController{
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! StockItemTableViewCell
         
         let stockItem: StockItem
-        if searchController.active && searchController.searchBar.text != "" {
-            stockItem = filteredStockItems[indexPath.row]
-        } else {
+        
+        //find out the state of the search terms to decide which cell it is
+        if searchController.active {
+            let searchBar = searchController.searchBar
+            let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+            
+            if searchBar.text == "" && scope != "ALL" {
+                stockItem = filteredStockItems[indexPath.row]
+            }
+            else if searchBar.text == "" && scope == "ALL" {
+                stockItem = stockItems[indexPath.row]
+            }
+            else if searchBar.text != "" && scope == "ALL" {
+                stockItem = filteredStockItems[indexPath.row]
+            }
+            else {
+                //searchBar.text != "" && scope != "ALL" is true
+                stockItem = filteredStockItems[indexPath.row]
+            }
+        }
+        else {
             stockItem = stockItems[indexPath.row]
         }
         
-        //self.profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 2;
-        //self.profileImageView.clipsToBounds = YES;
-
         
         // Configure the cell...
         cell.stockPhotoImageView.image = stockItem.photo
         cell.stockPhotoImageView.layer.cornerRadius = cell.stockPhotoImageView.frame.size.width / 2.0
-        
         cell.stockPhotoImageView.clipsToBounds = true
-        
-        
         
         cell.stockDescriptionLabel.text = stockItem.description.uppercaseString
         cell.stockFineDetailsLabel.text = "ID: " + String(stockItem.invCode) + " /// $" + String(stockItem.lastCost) + " per " + stockItem.units
@@ -324,22 +404,55 @@ class StocktakeTableViewController: UITableViewController{
                 let indexPath = tableView.indexPathForCell(selectedStockCell)!
                 let stockItem: StockItem
                 
+                
                 //must factor into the segue whether user selected after searching or no
-                if searchController.active && searchController.searchBar.text != "" {
-                    stockItem = filteredStockItems[indexPath.row]
-                } else {
+                if searchController.active {
+                    let searchBar = searchController.searchBar
+                    let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+                    
+                    if searchBar.text == "" && scope != "ALL" {
+                        stockItem = filteredStockItems[indexPath.row]
+                    }
+                    else if searchBar.text == "" && scope == "ALL" {
+                        stockItem = stockItems[indexPath.row]
+                    }
+                    else if searchBar.text != "" && scope == "ALL" {
+                        stockItem = filteredStockItems[indexPath.row]
+                    }
+                    else {
+                        //searchBar.text != "" && scope != "ALL" is true
+                        stockItem = filteredStockItems[indexPath.row]
+                    }
+                }
+                else {
                     stockItem = stockItems[indexPath.row]
                 }
                 
                 stockItemDetailViewController.stockItem = stockItem
             }
-        
-        
-        
         }
         
     }
 
+    
+    /*
+    //OLD NOT USED: kept as a reference for how to use paths and strings from url stuff
+    func getContentsOfURL() -> String? {
+        if let path = NSBundle.mainBundle().pathForResource("assets/stock_data", ofType: "csv") {
+            do {
+
+                return try String(contentsOfURL: NSURL(fileURLWithPath: path), encoding: NSUTF8StringEncoding) as String
+                //return try String(contentsOfFile: "stock_data", encoding:NSUTF8StringEncoding) as String
+            } catch {
+                print("caught a nil in getContentsOfURL")
+                return nil
+            }
+        }
+        else {
+            return nil
+        }
+    }
+    */
 }
 
 
