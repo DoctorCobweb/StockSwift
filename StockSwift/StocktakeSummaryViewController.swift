fba29d91b53653630030ae27d0f10f944b8ff45d
DoctorCobweb <spinninghalf@gmail.com>
2016-01-18 16:42:02 +1100

    implementing email functionality of stocktake

--- StockSwift/StocktakeSummaryViewController.swift
+++ StockSwift/StocktakeSummaryViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import MessageUI
 
-class StocktakeSummaryViewController: UIViewController {
+class StocktakeSummaryViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     //MARK: Properties
     var stocktake: Stocktake?
-
+    @IBOutlet weak var emailButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -28,6 +31,27 @@ class StocktakeSummaryViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func emailStocktake(sender: UIButton) {
+        print("emailStocktake")
+        
+        if MFMailComposeViewController.canSendMail() {
+            print("canSendMail is true")
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients(["andretrosky@gmail.com"])
+            mail.setMessageBody("yadda yadda yadda.", isHTML: false)
+            //mail.addAttachmentData(<#T##attachment: NSData##NSData#>, mimeType: <#T##String#>, fileName: <#T##String#>)
+            
+            presentViewController(mail, animated: true, completion: nil)
+        }
+        else {
+            print("ERROR: device cannot send email")
+        }
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismissViewControllerAnimated(true, completion: nil)
+    }
 
     // MARK: - Navigation
 
